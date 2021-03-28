59ef8917f7c3005a372cfdcfcab1ff4ad726f183
Nick Elliott <nickmartinelliott@gmail.com>
2021-03-28 18:31:54 -0400

    Local weather is now based on client coordinates instead of being hard-coded with a city value. Insects in area are fount by client input of a US state. The US states, where an insect is located, are now sent to client in insectJSON object. Insect response now waits until Flickr images are found and mapped to insectJSON object.

--- routes/index.js
+++ routes/index.js
@@ -8,147 +8,164 @@ const Dimension = require("../models").Dimension;
 const fetch = require('node-fetch');
 const config = require('../config.js')
 
+let locationID;
 let weatherJSON;
 let insectJSON = [];
-const services = [
-  {
-    type: "Build new screens",
-    desc: "Choose the color of your: frame, screen, hardware, etc.",
-    imgSrc: "border-outer.svg",
-    btn: "Build"
-  },
-  {
-    type: "Build new windows",
-    desc: "Choose the color of your: frame, screen, hardware, etc.",
-    imgSrc: "border-all.svg",
-    btn: "Build"
-  },
-  {
-    type: "Restore old screens",
-    desc: "Bring your house to life by repairing those old, dirty, torn screens.",
-    imgSrc: "layers-fill.svg",
-    btn: "Restore"
-  },
-  {
-    type: "Restore old windows",
-    desc: "Defeat the winter's cold air and revamp your old storm windows.",
-    imgSrc: "layers-half.svg",
-    btn: "Restore"
-  },
-  {
-    type: "Custom Glass",
-    desc: "Need glass? We can cut to size or shape depending on your needs.",
-    imgSrc: "columns-gap.svg",
-    btn: "Custom Glass"
-  },
-];
-const speciesBody = 
-      {
-        "criteriaType" : "species",
-        "locationCriteria": [{
-          "paramType" : "subnation",
-          "subnation" : "IN",
-          "nation" : "US"
-        }],
-        "pagingOptions" : {
-          "page" : 0,
-          "recordsPerPage" : 10000
-        },
-        "speciesTaxonomyCriteria": [
-          {
-            "paramType" : "scientificTaxonomy",
-            "level" : "CLASS",
-            "scientificTaxonomy" : "Arachnida",
-            "kingdom" : "Animalia"
-          },
-          {
-            "paramType" : "scientificTaxonomy",
-            "level" : "CLASS",
-            "scientificTaxonomy" : "Insecta",
-            "kingdom" : "Animalia"
-          },
-          {
-            "paramType" : "scientificTaxonomy",
-            "level" : "CLASS",
-            "scientificTaxonomy" : "Chilopoda",
-            "kingdom" : "Animalia"
-          },
-          {
-            "paramType" : "scientificTaxonomy",
-            "level" : "CLASS",
-            "scientificTaxonomy" : "Diplopoda",
-            "kingdom" : "Animalia"
-          }
-        ]
-      };
 
-//fetch APIs
-((async() => {
-
-  //Get local weather image from MetaWeather API
-  fetch('https://www.metaweather.com/api/location/2427032/', {
-    method: 'get',
-    headers: {'Content-Type':'application/json'},
-  })
-    .then(res => {
-      res.json().then(data => {
+ const getWeather = async (coords) => {
+    //Get location from MetaWeather API
+    await fetch(`https://www.metaweather.com/api/location/search/?lattlong=${coords.latitude},${coords.longitude}`, {
+      method: 'get',
+      headers: {'Content-Type':'application/json'},
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data[0]);
+        locationID = data[0].woeid;
+      }).catch(err => {
+        console.error("Fetching user coords from MetaWeather API: " + err)
+      });
+    //Get local weather from MetaWeather API  
+    fetch(`https://www.metaweather.com/api/location/${locationID}`, {
+      method: 'get',
+      headers: {'Content-Type':'application/json'},
+    })
+      .then(res => res.json())
+      .then(data => {
         console.log(data);
         weatherJSON = { weatherIMG: `https://www.metaweather.com/static/img/weather/${data.consolidated_weather[0].weather_state_abbr}.svg`};
-      })
-    });
+     }).catch(err => {
+      console.error("Fetching local weather from MetaWeather API: " + err)
+    });;
+ }     
 
+ //Get local insects
+const getInsects = (state) => {
+  return new Promise((resolve, reject) => { 
+    insectJSON = [];
+    const speciesBody = 
+    {
+      "criteriaType" : "species",
+      "locationCriteria": [{
+        "paramType" : "subnation",
+        "subnation" : state,
+        "nation" : "US"
+      }],
+      "pagingOptions" : {
+        "page" : 0,
+        "recordsPerPage" : 10000
+      },
+      "speciesTaxonomyCriteria": [
+        {
+          "paramType" : "scientificTaxonomy",
+          "level" : "CLASS",
+          "scientificTaxonomy" : "Arachnida",
+          "kingdom" : "Animalia"
+        },
+        {
+          "paramType" : "scientificTaxonomy",
+          "level" : "CLASS",
+          "scientificTaxonomy" : "Insecta",
+          "kingdom" : "Animalia"
+        },
+        {
+          "paramType" : "scientificTaxonomy",
+          "level" : "CLASS",
+          "scientificTaxonomy" : "Chilopoda",
+          "kingdom" : "Animalia"
+        },
+        {
+          "paramType" : "scientificTaxonomy",
+          "level" : "CLASS",
+          "scientificTaxonomy" : "Diplopoda",
+          "kingdom" : "Animalia"
+        }
+      ]
+    };
     //Get local insect species and push results to insectJSON array
-    await fetch('https://explorer.natureserve.org/api/data/speciesSearch', {
-            method: 'post',
-            body:    JSON.stringify(speciesBody),
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .then(res => res.json())
-        .then(async json => {    
-          const ranNums = [];  
-          // console.log(json);
-          //Create x number of random numbers between 0 and response results length    
-          for(let i = 0; i <= 59; i++){
-            let ranNum = parseInt(Math.random() * json.results.length);
-            ranNums.push(ranNum);
-          };
-          console.log(ranNums.length)
-          //Sort array from least to greatest
-          await ranNums.sort(function(a, b){return a-b});
-          //Randomly picks species result using number in ranNum array
-          json.results.map((species, i) => {
-            ranNums.map(num => {
+    fetch('https://explorer.natureserve.org/api/data/speciesSearch', {
+      method: 'post',
+      body:    JSON.stringify(speciesBody),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(async json => {    
+        const ranNums = [];  
+        //Create x number of random numbers between 0 and response results length    
+        for(let i = 0; i <= 59; i++){
+          let ranNum = parseInt(Math.random() * json.results.length);
+          ranNums.push(ranNum);
+        };
+        //Sort array from least to greatest
+        ranNums.sort(function(a, b){return a-b});
+        //Randomly picks species result using number in ranNum array, resolves promise after cycling results array
+        json.results.map((species, i) => {
+          if(i === (json.results.length - 1)){
+            resolve(insectJSON);
+          }else{
+            ranNums.map(async num => {
               if(num === i){
-                insectJSON.push({commonName: species.primaryCommonName, sciName: species.scientificName, url: species.nsxUrl, photo: []});
+                const USStates = [];
+                await species.nations.map(nation => {
+                  if(nation.nationCode === "US"){
+                    USStates.push(nation.subnations);
+                  }
+                });
+                insectJSON.push({commonName: species.primaryCommonName, sciName: species.scientificName, url: species.nsxUrl, subNations: USStates, photo: []});
                 ranNums.shift();
               }
             });
-          });
-        }); 
-
-     
-    //Uses mapped insectJSON array and Flickr API to find image of species and map src back to insectJSON array
-    insectJSON.map((insect, i) => {
-      // console.log(insectJSON);
-      if(i <= 59){
-        fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config}&tags=${insect.sciName}&license=1,2,3,5&per_page=24&extras=url_m&format=json&nojsoncallback=1`)
-        .then(res => res.json())
-        .then(json => {
-          if(json.photos.photo[0]){
-            insectJSON[i].photo.push(json.photos.photo[0].url_m ? json.photos.photo[0].url_m : "");
           }
-        });  
-      }    
-    });     
-})())
+        });
+      }).catch(err => {
+        console.log("Fetching Insect Species: " + err);
+      }); 
+  }).catch(err => {
+    console.log("getInsects function: " + err);
+  });
+};  
 
-/* GET home page. */
+const getInsectPhoto = (insectArry) => {
+  console.log(insectArry[0]);
+  const promises = insectArry.map((insect, i) => {
+    return fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${config}&tags=${insect.sciName}&license=1,2,3,5&per_page=24&extras=url_m&format=json&nojsoncallback=1`)
+      .then(res => res.json())
+      .then(json => {
+        json.photos.photo[0] ? insectJSON[i].photo.push(json.photos.photo[0].url_m) : " ";
+      }).catch(err => {
+        console.error("Fetching insect photo: " + err);
+      });
+  });
+  return Promise.all(promises);
+};
+
+//Get weather
 router.get('/weather', function(req, res, next) {  
   res.json(weatherJSON);
 });
 
-router.get('/insects', function(req, res, next) {  
-  res.json(insectJSON);
+router.post('/', function(req, res, next) {  
+  Promise.resolve().then(() => {
+    coords = req.body;
+    getWeather(req.body);
+    res.status(200).end();
+  }).catch(next);
+});
+
+router.post('/insects', function(req, res, next) {  
+  Promise.resolve().then(() => {
+    getInsects(req.body.state)
+    .then(insectArry => {
+      getInsectPhoto(insectArry)
+        .then(()=>{
+          res.json(insectJSON);
+          res.status(200).end();
+        }).catch( err => {
+          res.status(500).end();
+          throw new Error("getInsectPhoto function: " + err);
+        });
+    });
+  }).catch(next);
 });
 
 
