ecb32e430ca1affe8c52d093968bf3788fbe4f8a
Nick Elliott <nickmartinelliott@gmail.com>
2021-04-03 20:20:12 -0400

    Commenting out unused view engine code for testing purposes.

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ var orderRouter = require('./routes/orders');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+// app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+// app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd')));
+// app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+// app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 
 app.use('/', indexRouter);
