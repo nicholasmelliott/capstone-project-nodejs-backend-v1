6098b0b4813bde31affc17e0f5706d3a2b465098
Nick Elliott <nickmartinelliott@gmail.com>
2021-05-05 21:20:22 -0400

    Modulized services.

---
+++ services.js
@@ -0,0 +1,39 @@
+const services = [
+    {
+      type: "Build new screens",
+      desc: "Choose the color of your: frame, screen, hardware, etc.",
+      imgSrc: "border-outer.svg",
+      btn: "Build",
+      href: "bScreens"
+    },
+    {
+      type: "Build new windows",
+      desc: "Choose the color of your: frame, screen, hardware, etc.",
+      imgSrc: "border-all.svg",
+      btn: "Build",
+      href: "bWindows"
+    },
+    {
+      type: "Restore old screens",
+      desc: "Bring your house to life by repairing those old, dirty, torn screens.",
+      imgSrc: "layers-fill.svg",
+      btn: "Restore",
+      href: "rScreens"
+    },
+    {
+      type: "Restore old windows",
+      desc: "Defeat the winter's cold air and revamp your old storm windows.",
+      imgSrc: "layers-half.svg",
+      btn: "Restore",
+      href: "rWindows"
+    },
+    {
+      type: "Custom Glass",
+      desc: "Need glass? We can cut to size or shape depending on your needs.",
+      imgSrc: "columns-gap.svg",
+      btn: "Custom Glass",
+      href: "cGlass"
+    },
+  ];
+
+  module.exports = services;
