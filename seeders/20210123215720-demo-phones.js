d771bdaa397de28f098962b38b4f1e7a03d74b8a
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-10-14 15:50:59 -0400

    Update Phones seed file to dynamically populate personId and companyId
    Modified the Phones seed file to query actual IDs from Persons and Companies tables. Separated phone entries into two arrays based on whether they should have a personId or companyId, and populated these fields dynamically before seeding. This resolves foreign key constraint issues.

--- seeders/20210123215720-demo-phones.js
+++ seeders/20210123215720-demo-phones.js
@@ -2,60 +2,84 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Phones', [{
-        personId: 1,
-        areaCd: '481',
-        number: '5162342',
-        type: 'cell',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     },
-     {
-        personId: 6,
-        areaCd: '555',
-        number: '1111111',
-        type: 'cell',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     },
-     {
-        personId: 6,
-        areaCd: '555',
-        number: '2222222',
-        type: 'home',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     },
-     {
-        personId: 2,
-        areaCd: '555',
-        number: '0000000',
-        type: 'cell',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     },
-     {
-        companyId: 1,
-        areaCd: '555',
-        number: '5555552',
-        type: 'home',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     },
-     {
-        companyId: 2,
-        areaCd: '555',
-        number: '5555551',
-        type: 'cell',
-        createdAt: new Date(),
-        updatedAt: new Date()
-     }
-    ], {});
+    // Query actual IDs from the Person and Company tables
+    const [persons] = await queryInterface.sequelize.query("SELECT id FROM People");
+    const [companies] = await queryInterface.sequelize.query("SELECT id FROM Companies");
+
+    // Original phone data meant to have personId
+    const phonesForPersons = [
+      {
+         areaCd: '481',
+         number: '5162342',
+         type: 'cell',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      },
+      {
+         areaCd: '555',
+         number: '1111111',
+         type: 'cell',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      },
+      {
+         areaCd: '555',
+         number: '2222222',
+         type: 'home',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      },
+      {
+         areaCd: '555',
+         number: '0000000',
+         type: 'cell',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      }
+    ];
+
+    // Original phone data meant to have companyId
+    const phonesForCompanies = [
+      {
+         areaCd: '555',
+         number: '5555552',
+         type: 'home',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      },
+      {
+         areaCd: '555',
+         number: '5555551',
+         type: 'cell',
+         createdAt: new Date(),
+         updatedAt: new Date()
+      }
+    ];
+
+    // Map phone data with actual personId
+    const phonesForPersonsMapped = phonesForPersons.map((phoneData, index) => {
+      return {
+        personId: persons[index % persons.length].id,
+        ...phoneData
+      };
+    });
+
+    // Map phone data with actual companyId
+    const phonesForCompaniesMapped = phonesForCompanies.map((phoneData, index) => {
+      return {
+        companyId: companies[index % companies.length].id,
+        ...phoneData
+      };
+    });
+
+    // Combine both arrays
+    const phonesData = [...phonesForPersonsMapped, ...phonesForCompaniesMapped];
+    
+    // Now seed Phones table
+    await queryInterface.bulkInsert('Phones', phonesData);
   },
 
   down: async (queryInterface, Sequelize) => {
-
     await queryInterface.bulkDelete('Phones', null, {});
-
   }
 };
